38255f092666661504c6d607ebafdd11036a3f8d
devseme <semeochenge@gmail.com>
2023-01-28 14:16:11 +0300

    did first version of models

--- biashara/admin.py
+++ biashara/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Borrower)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Amount)
+admin.site.register(Business_sector)
--- biashara/models.py
+++ biashara/models.py
@@ -1,7 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
+from django.core.validators import RegexValidator
 
 # Create your models here.
-class Borrower(models.Model):
+class Profile(models.Model):
+    user =models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=50 ,null=True, blank=True)
     email = models.EmailField(max_length=255, unique =True)
     phone_number = models.CharField(max_length=10,unique=True,blank=True) 
+    
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.save()   
+        
+    def __str__(self):
+        return self.user.username   
+class Business_sector(models.Model):
+    description =models.TextField 
+    
+    def __str__(self):
+         return self.description 
+class Amount(models.Model):
+       amount = models.DecimalField(max_digits=6, decimal_places=2)
+       reason = models.TextField(max_length=250, blank=True)
+       
+       
+       def __str__(self):
+            return self.reason      
+
+class Business(models.Model):
+        business_name = models.CharField(max_length=50 ,null=True, blank=True)
+        business_address = models.CharField(max_length=15 ,null=True, blank=True)
+        description = models.TextField(max_length=250, blank=True)
+        company_number = models.PositiveIntegerField(max_length=8 ,null=True, blank=True,unique=True ,validators=[RegexValidator(r'^\d{1,10}$')])
+        Business_sector = models.ManyToManyField(Business_sector, related_name='business_sector')
+        def save_business(self):
+            self.save()
+     
+        def delete_business(self):
+             self.delete()
+             
+        def __str__(self):
+            return self.description     
+
+             
+             
+
+        
+    
+    
+    
+    
+    
