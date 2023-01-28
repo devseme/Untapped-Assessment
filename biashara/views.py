af7756f218dd96975f269ccc33ab77b98b024cf4
devseme <semeochenge@gmail.com>
2023-01-28 16:35:55 +0300

    rendered and started on the sign up form

--- biashara/views.py
+++ biashara/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .forms import signup_form
 
 # Create your views here.
+
+def home(request):
+    context = { }
+    return render(request, 'index.html' ,context)
+
+    
+    
+def register_page(request):
+    form = signup_form()
+    
+    
+    if request.method =="POST":
+         form = signup_form(request.POST)
+         if form.is_valid():
+             form.save()
+             
+        
+    context = {'form':form }
+    return render(request, 'accounts/registration.html' ,context)
+
+def login_page(request):
+    context = { }
+    return render(request, 'accounts/login.html',context)
