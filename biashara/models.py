765daf00210821a79a8144b19abe42e800aa9420
devseme <semeochenge@gmail.com>
2023-01-28 13:32:19 +0300

    ran first test for admin page

--- biashara/models.py
+++ biashara/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Borrower(models.Model):
+    name = models.CharField(max_length=50 ,null=True, blank=True)
+    email = models.EmailField(max_length=255, unique =True)
+    phone_number = models.CharField(max_length=10,unique=True,blank=True) 
